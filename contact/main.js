9eb5edbe2e15bf7fa04d1ba301cf001aa51de61e
Ganji Siva Prakash <ganjisivaprakash@users.noreply.github.com>
2016-03-01 04:35:10 +0530

    uploaded

---
+++ contact/main.js
@@ -0,0 +1,160 @@
+$(window).load(function(){
+		$(".num1").hover(function () {
+			$('#top1').stop();
+			$('#top1').show();
+
+		}, function () {
+			 $('#top1').stop();
+			$('#top1').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num2").hover(function () {
+			$('#top2').stop();
+			$('#top2').show();
+
+		}, function () {
+			 $('#top2').stop();
+			$('#top2').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num3").hover(function () {
+			$('#top3').stop();
+			$('#top3').show();
+
+		}, function () {
+			 $('#top3').stop();
+			$('#top3').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num4").hover(function () {
+			$('#top4').stop();
+			$('#top4').show();
+
+		}, function () {
+			 $('#top4').stop();
+			$('#top4').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num5").hover(function () {
+			$('#top5').stop();
+			$('#top5').show();
+
+		}, function () {
+			 $('#top5').stop();
+			$('#top5').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num6").hover(function () {
+			$('#top6').stop();
+			$('#top6').show();
+
+		}, function () {
+			 $('#top6').stop();
+			$('#top6').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num7").hover(function () {
+			$('#top7').stop();
+			$('#top7').show();
+
+		}, function () {
+			 $('#top7').stop();
+			$('#top7').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num8").hover(function () {
+			$('#top8').stop();
+			$('#top8').show();
+
+		}, function () {
+			 $('#top8').stop();
+			$('#top8').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num9").hover(function () {
+			$('#top9').stop();
+			$('#top9').show();
+
+		}, function () {
+			 $('#top9').stop();
+			$('#top9').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num10").hover(function () {
+			$('#top10').stop();
+			$('#top10').show();
+
+		}, function () {
+			 $('#top10').stop();
+			$('#top10').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num11").hover(function () {
+			$('#top11').stop();
+			$('#top11').show();
+
+		}, function () {
+			 $('#top11').stop();
+			$('#top11').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num12").hover(function () {
+			$('#top12').stop();
+			$('#top12').show();
+
+		}, function () {
+			 $('#top12').stop();
+			$('#top12').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num13").hover(function () {
+			$('#top13').stop();
+			$('#top13').show();
+
+		}, function () {
+			 $('#top13').stop();
+			$('#top13').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num14").hover(function () {
+			$('#top14').stop();
+			$('#top14').show();
+
+		}, function () {
+			 $('#top14').stop();
+			$('#top14').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num15").hover(function () {
+			$('#top15').stop();
+			$('#top15').show();
+
+		}, function () {
+			 $('#top15').stop();
+			$('#top15').hide();
+		});
+		});//]]> 
+$(window).load(function(){
+		$(".num16").hover(function () {
+			$('#top16').stop();
+			$('#top16').show();
+
+		}, function () {
+			 $('#top16').stop();
+			$('#top16').hide();
+		});
+		});//]]> 
